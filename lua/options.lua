f357d3feea6b2ce8ccc272d015c7383414219823
Felix Engel <felix.justus.engel@rwth-aachen.de>
2024-11-18 16:48:10 +0100

    Update options

--- lua/options.lua
+++ lua/options.lua
@@ -78,7 +78,7 @@ vim.o.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+vim.opt.confirm = true
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
