4c70815b71425854a0471aa8fa371533cba01e73
Felix Engel <felix.justus.engel@rwth-aachen.de>
2024-11-19 12:21:06 +0100

    feat(keymaps): Helpful visual mode bindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,13 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+local keymap = vim.keymap.set
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+keymap('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -14,22 +15,34 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap('n', '<C-left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap('n', '<C-right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap('n', '<C-down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap('n', '<C-up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Keymaps for better default experience
+-- Remap for dealing with word wrap
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- Remap for dealing with word wrap
+keymap('n', 'Up', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap('n', 'Down', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- [[ Visual Mode ]]
+
+-- Stay in indent mode
+keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+keymap('v', '<', '<gv', { silent = true })
+keymap('v', '>', '>gv', { silent = true })
+
+-- Better paste - keep register
+keymap('v', 'p', '"_dP', { silent = true })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
